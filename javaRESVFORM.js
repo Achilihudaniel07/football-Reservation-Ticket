3b86935f287b00ce4d82f1bd8e401ad21fbf7467
unknown <achilihudaniel53@gmail.com>
2022-07-19 17:00:33 +0100

    second commit

--- javaRESVFORM.js
+++ javaRESVFORM.js
@@ -31,9 +31,10 @@
   // To change eplclubs background
   var epl = document.querySelectorAll("option")[5].parentElement;
   epl.addEventListener("click", function(){
-  document.querySelector("form").style.background="url('epl.jpeg')";
+  document.querySelector("body").style.background="url('epl.jpeg')";
+  document.querySelector("form").style.color="black"
   document.querySelector("form").style.fontSize="1.2rem"; 
-  document.querySelector("form").style.color="red"
+ 
   });
 
 
@@ -43,8 +44,7 @@
   // To change tornament background
   var epl = document.querySelectorAll("input")[4]
   epl.addEventListener("click", function(){
-  document.querySelector("form").style.backgroundImage="url('epl2.jpeg')";
-  document.querySelector("b")[6].style.backgroundColor="yellow";
+  document.querySelector("body").style.backgroundImage="url('epl2.jpeg')";
   document.querySelector("form").style.color="black";
   document.querySelector("form").style.fontSize="1.2rem";
   });
@@ -52,27 +52,24 @@
 
   var cl = document.querySelectorAll("input")[5];
   cl.addEventListener("click", function(){
-  document.querySelector("form").style.backgroundImage="url('cl2.jpeg')";
-  document.querySelectorAll("b")[6].style.backgroundColor="blue"
-  document.querySelector("form").style.color="white"
+  document.querySelector("body").style.backgroundImage="url('cl2.jpeg')";
+  document.querySelector("form").style.color="black"
   document.querySelector("form").style.fontSize="1.2rem"
   });
 
 
   var fa = document.querySelectorAll("input")[6]
   fa.addEventListener("click", function(){
-  document.querySelector("form").style.backgroundImage="url('facup2.jpeg')";
-  document.querySelectorAll("b")[6].style.backgroundColor="blue"
-  document.querySelector("form").style.color="white"
+  document.querySelector("body").style.backgroundImage="url('facup2.jpeg')";
+  document.querySelector("form").style.color="black"
   document.querySelector("form").style.fontSize="1.2rem"
   });
 
 
   var carl = document.querySelectorAll("input")[7]
   carl.addEventListener("click", function(){
-  document.querySelector("form").style.backgroundImage="url('carl.jpeg')";
-  document.querySelectorAll("b")[6].style.backgroundColor="black"
-  document.querySelector("form").style.color="gold"
+  document.querySelector("body").style.backgroundImage="url('carl.jpeg')";
+  document.querySelector("form").style.color="black"
   document.querySelector("form").style.fontSize="1.2rem"
   });
 
@@ -83,8 +80,9 @@
   //  To change button effect
   var button = document.getElementById("matchday");
   button.addEventListener("mouseenter", function(){
-  document.querySelector("form").style.background="blue"
-  document.querySelector("form").style.border="15px dotted black"
+  document.querySelector("form").style.background="black"
+  document.querySelector("form").style.color="white"
+  document.querySelector("form").style.border="15px dotted white"
   document.querySelector("form").style.borderRadius="15rem"
   document.querySelector("button").style.borderRadius="35rem"
   document.querySelector("button").style.backgroundColor="red"
@@ -92,10 +90,11 @@
 
   var button = document.getElementById("matchday");
   button.addEventListener("mouseleave", function(){
-  document.querySelector("form").style.background="gold"
+  document.querySelector("form").style.background="white"
+  document.querySelector("form").style.color="black"
   document.querySelector("form").style.border="15px solid black"
   document.querySelector("form").style.borderRadius="0"
   document.querySelector("button").style.backgroundColor="whitesmoke"
   });
    
-  // #################################################################################################################
+  // ################################################################################################################
